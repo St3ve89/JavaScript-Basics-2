b56d43b0dc3717d2de0fc3b5c133626724dfd8ff
St3ve89 <istvan.acs89@gmail.com>
2018-03-29 13:02:11 +0100

    Using console for debugging and how to fix problems

--- src/JavaScript/js.js
+++ src/JavaScript/js.js
@@ -1,1 +1,3 @@
 document.write("This is our javaScript being written first before anything else");
+
+console.log("This is text that will be sent directly to the console to let us know that our program completed succesfully");
